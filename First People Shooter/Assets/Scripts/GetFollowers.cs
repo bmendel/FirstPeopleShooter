d55c40599b6bd72497e8c3e5db4a35667cc340dd
Eric Gonzalez Olivares <gonz9019@csumb.ad.csumb.edu>
2018-11-30 22:05:42 -0800

    Created Stats and getFollowers scripts

---
+++ First People Shooter/Assets/Scripts/GetFollowers.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetFollowers : MonoBehaviour {
+
+    public int newFollowers;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Stats>().currentFollowers += newFollowers;
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.tag == "Temple")
+        {
+            newFollowers = Random.Range(1, 10);
+        }
+        else if(collision.tag == "Shrine")
+        {
+            newFollowers = Random.Range(1, 4);
+        }
+        if (newFollowers > 20)
+            newFollowers = 20;
+    }
+}
---
+++ First People Shooter/Assets/Scripts/Stats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour {
+
+    public int Influence;
+    public int currentFollowers;
+    public int usedFollowers;
+    public int playerHealth;
+    public int ownedTemples;
+
+    // Use this for initialization
+    void Start () {
+        playerHealth = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Influence = currentFollowers - usedFollowers;
+
+	}
+}
