38010b49c46457948906bd9981624518db1f68be
Eric Gonzalez Olivares <gonz9019@csumb.ad.csumb.edu>
2018-12-01 16:37:15 -0800

    Added Destrying Enemies

---
+++ First People Shooter/Assets/Scripts/DestroyEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemy : MonoBehaviour {
+
+    public GameObject enemy;
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            //GetComponent<EnemyWaveManager>().enemy_count--;
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+}
